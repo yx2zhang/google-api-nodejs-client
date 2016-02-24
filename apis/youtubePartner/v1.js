f50cbff93704e5995c8cffbd086ced9cc6def9f7
yx2zhang <zhangyangxu@gmail.com>
2016-02-24 00:54:13 -0800

    updated des

--- apis/youtubePartner/v1.js
+++ apis/youtubePartner/v1.js
@@ -28,7 +28,7 @@ function Youtubepartner(options) {
      *
      * @param  {object} params - Parameters for request
      * @param  {string} params.id - The Youtube channel id.
-     * @param  {string} params.onBehalfOfContentOwner - Note: This parameter is intended exclusively for YouTube content Youtube channel iYoutube channel identOwner parameter indicates that the request's authorization credentials identify a YouTube CMS user who is acting on behalf of the content owner specified in the parameter value. This parameter is intended for YouTube content partners that own and manage many different YouTube channels. It allows content owners to authenticate once and get access to all their video and channel data, without having to provide authentication credentials for each individual channel. The actual CMS account that the user authenticates with must be linked to the specified YouTube content owner.
+     * @param  {string} params.onBehalfOfContentOwner - The onBehalfOfContentOwner parameter identifies the content owner that the user is acting on behalf of. This parameter supports users whose accounts are associated with multiple content owners.
      * @param  {callback} callback - The callback that handles the response.
      * @return {object} Request object
      */
